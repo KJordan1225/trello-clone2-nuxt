d73f6df8338733f38b95973365d1ed8ebedbc828
Keith <shadow902@gmail.com>
2023-12-14 20:54:46 -0500

    add signin and signout pages

---
+++ pages/auth/signout.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Sign Out</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-row justify="center" align="center">
+  <h1>Keith's Trello Clone</h1>
+  <!-- <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
@@ -73,11 +74,10 @@
         </v-card-actions>
       </v-card>
     </v-col>
-  </v-row>
+  </v-row> -->
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
 }
 </script>
